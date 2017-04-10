6aef1e920ce9f4cfb1c2707ab4bc8624ed2fa227
Ryszard-Ps <textex204@gmail.com>
2017-04-10 16:29:15 -0500

    add configuration

--- raiz/raiz.php
+++ raiz/raiz.php
@@ -19,10 +19,10 @@ $logo = '<img src="vistas/imagenes/logo.png" border="0" width="110" height="30">
 session_start();
 
 # constantes de base de datos
-define('DB_SEVIDOR', 'localhost');
-define('DB_USUARIO', 'root');
-define('DB_CLAVE', 'root');
-define('DB_NOMBRE', 'koala');
+define('DB_SEVIDOR', getenv('BD_SERVIDOR'));
+define('DB_USUARIO', getenv('BD_USUARIO'));
+define('DB_CLAVE', getenv('BD_CLAVE'));
+define('DB_NOMBRE', getenv('BD_NOMBRE'));
 $fecha =  date('\F\e\c\h\a \d\e \e\m\i\s\i\ó\n: Y/m/d  \d\e\l H:i:s');
 # constantes directorios
 define('HTML_DIR','html/');
